5b8924fa78ba4a828a9a3cd5b212a35a0bec6b86
JulioSpassky <juliocto2015@gmail.com>
2021-02-22 22:42:20 -0300

    :rocket: i18n spring

--- i18n/SpringBoot/i18n/src/main/java/com/example/i18n/controller/InternationalizationController.java
+++ i18n/SpringBoot/i18n/src/main/java/com/example/i18n/controller/InternationalizationController.java
@@ -1,14 +1,12 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/i18n")
-public class internationalizationController {
+public class InternationalizationController {
 
-    @GetMapping()
+    @GetMapping("/hello")
     public String get() {
         return "A";
     }
---
+++ i18n/SpringBoot/i18n/src/main/java/com/example/i18n/i18n/Messages.java
@@ -0,0 +1,2 @@
+package com.example.i18n.i18n;public class Messages {
+}
